e85438275ec45e38c93af96193b2929ce6b85690
Daniel Landau <daniel.landau@futurice.com>
2017-02-07 15:33:34 +0200

    Update copy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Tervetuloa Tradenomiittiin. Kysy. Löydä. Kohtaa.');
+  res.send('Tervetuloa Tradenomiittiin.');
 });
 
 app.listen(3000, () => {
