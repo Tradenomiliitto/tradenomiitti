73d0118533d6a56905b0505146ebf710cc922898
Daniel Landau <daniel.landau@futurice.com>
2017-02-09 13:13:28 +0200

    Serve index.html on all paths

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const rootDir = "../frontend"
 
 const app = express();
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile('./index.html', {root: rootDir})
 });
 
