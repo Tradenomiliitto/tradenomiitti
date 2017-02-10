b3ca28a4b3eb13334bcde35e19c49dd667510f09
Topi Paavilainen <topi.paavilainen@futurice.com>
2017-02-10 10:48:53 +0200

    connect database to server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 
 const rootDir = "./frontend"
-
 const app = express();
+const knex_config = require('../knexfile.js')
+const knex = require('knex')(knex_config.local)
+
+app.get('/api/user/:id', (req, res) => {
+  knex('users').where('id', req.params.id)
+    .then(function(rows){
+      if(rows.length === 0){
+         return Promise.reject("Not Found");
+      }
+      else return rows;
+    })
+    .then(rows => res.send(rows[0]))
+    .catch(e => res.sendStatus(404))
+});
+
 app.get('*', (req, res) => {
   res.sendFile('./index.html', {root: rootDir})
 });
