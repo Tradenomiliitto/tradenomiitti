717633d789173d8e59980476eb66dd5dc55fd0e3
Daniel Landau <daniel.landau@futurice.com>
2017-02-10 13:55:50 +0200

    Fix syntax

--- migrations/20170209170424_users.js
+++ migrations/20170209170424_users.js
@@ -1,15 +1,12 @@
 
 exports.up = function(knex, Promise) {
-    return Promise.all(
-        knex.schema.createTable('users', function(table){
-        table.increments('id');
-        table.string('first_name');
-        table.string('description');
-    })).then(console.log("table created"))
+  return knex.schema.createTable('users', function(table){
+    table.increments('id');
+    table.string('first_name');
+    table.string('description');
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return Promise.all(
-        knex.schema.dropTable('users')
-    ).then(console.log("table dropped"))
+  return knex.schema.dropTable('users');
 };
