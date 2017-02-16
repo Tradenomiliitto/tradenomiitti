3294bfc6a21bd1ecefdc599c3dad41a1da4bcf47
Topi Paavilainen <topi.paavilainen@futurice.com>
2017-02-16 10:38:01 +0200

    init modifies url insted of newUrl, FetchUser removed, some comments & indentation

--- frontend/User.elm
+++ frontend/User.elm
@@ -6,55 +6,52 @@ import Http
 import Html exposing (Html, div, text)
 
 type alias User = 
-    {
-        name: String,
-        description: String
-    }
+  {
+    name: String,
+    description: String
+  }
 
 userDecoder : Decoder User
 userDecoder =
-    decode User
-        |> required "first_name" string
-        |> required "description" string
+  decode User
+    |> required "first_name" string
+    |> required "description" string
 
 -- UPDATE 
 
-type Msg = FetchUser Int | UpdateUser (Result Http.Error User)
+type Msg = UpdateUser (Result Http.Error User)
 
 update : Msg -> Maybe User -> ( Maybe User, Cmd Msg)
 update msg user =
-    case msg of
-        FetchUser userId ->
-            (user, getUser userId)
-
-        UpdateUser (Ok updatedUser) ->
-            (Just updatedUser, Cmd.none) 
-
-        UpdateUser (Err _) -> 
-            (Nothing, Cmd.none)
+  case msg of
+    UpdateUser (Ok updatedUser) ->
+      (Just updatedUser, Cmd.none) 
+    -- TODO: show error instead of spinning in case of user not found
+    UpdateUser (Err _) -> 
+      (Nothing, Cmd.none)
 
 getUser : Int -> Cmd Msg
 getUser userId =
-    let 
-        url = "/api/user/" ++ (toString userId)
-        request = Http.get url userDecoder
-    in
-        Http.send UpdateUser request
+  let 
+    url = "/api/user/" ++ (toString userId)
+    request = Http.get url userDecoder
+  in
+    Http.send UpdateUser request
 
 
 -- VIEW
 
 view : Maybe User -> Html Msg 
 view userMaybe =
-    userMaybe
-        |> Maybe.map viewUser
-        |> Maybe.withDefault (div [] [text "spinning"])
+  userMaybe
+    |> Maybe.map viewUser
+    |> Maybe.withDefault (div [] [text "spinning"])
 
   
 viewUser : User -> Html Msg
 viewUser user =
-    div [] 
-    [
-        div [] [ text user.name ],
-        div [] [ text user.description ]
-    ]
+  div [] 
+  [
+    div [] [ text user.name ],
+    div [] [ text user.description ]
+  ]
