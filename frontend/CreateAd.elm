942951e40db31f205c2a428382a2b282b9faa71b
Daniel Landau <daniel.landau@futurice.com>
2017-02-28 17:49:53 +0200

    Add ad creation layout

--- frontend/CreateAd.elm
+++ frontend/CreateAd.elm
@@ -1,9 +1,36 @@
 module CreateAd exposing (..)
 
 import Html as H
+import Html.Attributes as A
 
 type Msg = NoOp
 
 view : H.Html Msg
 view =
-  H.div [] []
+  H.div
+  [ A.class "container"]
+  [ H.div
+    [ A.class "row" ]
+    [ H.div
+      [ A.class "col-xs-12 col-sm-7" ]
+      [ H.h2
+          []
+          [ H.input
+            [ A.placeholder "Otsikko" ]
+            []
+          ]
+      , H.textarea
+        [ A.placeholder "Kirjoita ytimekäs ilmoitus" ]
+        []
+      ]
+    , H.div
+      [ A.class "col-xs-12 col-sm-5" ]
+      [ H.h3 [] [ H.text "Kenen toivot vastaavan?" ]
+      , H.p [] [ H.text "Vastaajatarjokkaiden haku on tulossa pian" ]
+      , H.p [] [ H.button
+                [ A.class "btn btn-primary" ]
+                [ H.text "Julkaise ilmoitus"]
+               ]
+      ]
+    ]
+  ]
