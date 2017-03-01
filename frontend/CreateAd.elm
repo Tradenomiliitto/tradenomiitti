7a4c20e68865f86cd9608736fc582003acc2f226
Daniel Landau <daniel.landau@futurice.com>
2017-03-01 10:26:35 +0200

    Add styling

--- frontend/CreateAd.elm
+++ frontend/CreateAd.elm
@@ -10,27 +10,33 @@ view =
   H.div
   [ A.class "container"]
   [ H.div
-    [ A.class "row" ]
+    [ A.class "row create-ad" ]
     [ H.div
-      [ A.class "col-xs-12 col-sm-7" ]
+      [ A.class "col-xs-12 col-sm-7 create-ad__inputs" ]
       [ H.h2
-          []
+          [ A.class "create-ad__heading-input" ]
           [ H.input
             [ A.placeholder "Otsikko" ]
             []
           ]
       , H.textarea
-        [ A.placeholder "Kirjoita ytimekäs ilmoitus" ]
+        [ A.placeholder "Kirjoita ytimekäs ilmoitus"
+        , A.class "create-ad__textcontent"
+        ]
         []
       ]
     , H.div
       [ A.class "col-xs-12 col-sm-5" ]
-      [ H.h3 [] [ H.text "Kenen toivot vastaavan?" ]
+      [ H.h3
+          [ A.class "create-ad__filters-heading"]
+          [ H.text "Kenen toivot vastaavan?" ]
       , H.p [] [ H.text "Vastaajatarjokkaiden haku on tulossa pian" ]
-      , H.p [] [ H.button
-                [ A.class "btn btn-primary" ]
-                [ H.text "Julkaise ilmoitus"]
-               ]
+      , H.p
+        [ A.class "create-ad__submit-button" ]
+        [ H.button
+            [ A.class "btn btn-primary" ]
+            [ H.text "Julkaise ilmoitus"]
+        ]
       ]
     ]
   ]
