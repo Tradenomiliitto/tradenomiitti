ff24beb4d59b14fc8e9528713ed60d2c3d5ef680
Daniel Landau <daniel.landau@futurice.com>
2017-03-09 09:32:59 +0200

    Extract logon handling to separate file

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const request = require('request');
 const cookieSession = require('cookie-session');
 
 const sebacon = require('./sebaconService');
+const logon = require('./logonHandling');
 
 const rootDir = "./frontend"
 
@@ -62,80 +63,14 @@ if (!sebaconAuth ||
 sebacon.initialize({ customer: sebaconCustomer, user: sebaconUser,
                      password: sebaconPassword, auth: sebaconAuth});
 
+logon.initialize({ communicationsKey, knex, sebacon });
+
 const urlEncoded = bodyParser.urlencoded();
 const jsonParser = bodyParser.json();
 
-app.post('/login', urlEncoded, (req, res) => {
-  const ssoId = req.body.ssoid;
-  const validationReq = {
-    id: uuid.v4(),
-    method: "GetUser",
-    params: [
-      communicationsKey,
-      ssoId
-    ],
-    jsonrpc: "2.0"
-  };
-  request.post({
-    url: 'https://tunnistus.avoine.fi/mmserver',
-    json: validationReq
-  }, (err, response, body) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Jotain meni pieleen');
-    }
+app.post('/login', urlEncoded, logon.login );
 
-    if (body.error) {
-      console.log(body);
-      req.session = null;
-      return res.status(400).send('Kirjautuminen epäonnistui');
-    }
-
-    const sessionId = uuid.v4();
-    const remoteId = body.result.local_id;
-    return knex('users').where({ remote_id: remoteId })
-      .then((resp) => {
-        if (resp.length === 0) {
-          return Promise.all([
-            sebacon.getUserFirstName(remoteId),
-            sebacon.getUserNickName(remoteId),
-            sebacon.getUserPositions(remoteId),
-            sebacon.getUserDomains(remoteId)
-          ]).then(([firstname, nickname, positions, domains]) => {
-            return knex('users')
-              .insert({
-                remote_id: body.result.local_id,
-                data: {
-                  name: nickname || firstname,
-                  domains: domains.map(d => ({ heading: d, skill_level: 1 })),
-                  positions: positions.map(d => ({ heading: d, skill_level: 1 })),
-                  profile_creation_consented: false
-                }
-              }, 'id') // postgres does not automatically return the id, ask for it explicitly
-          })
-            .then(insertResp => ({ id: insertResp[0] }))
-        } else {
-          return resp[0];
-        }
-      })
-      .then((user) => {
-        return knex('sessions').insert({
-          id: sessionId,
-          user_id: user.id
-        }).then(() => {
-          req.session.id = sessionId;
-          return res.redirect(req.query.path || '/');
-        });
-      })
-  });
-});
-
-app.get('/logout', (req, res) => {
-  const sessionId = req.session.id || 'nosession';
-  req.session = null;
-  return knex('sessions').where({id: sessionId}).del()
-    .then(() => res.redirect('https://tunnistus.avoine.fi/sso-logout/'));
-});
+app.get('/logout', logon.logout);
 
 app.get('/api/me', (req, res) => {
   if (!req.session || !req.session.id) {
---
+++ server/logonHandling.js
@@ -0,0 +1,88 @@
+const uuid = require('uuid');
+const request = require('request');
+
+let communicationsKey, knex, sebacon;
+
+function initialize(params) {
+  communicationsKey = params.communicationsKey;
+  knex = params.knex;
+  sebacon = params.sebacon;
+}
+
+function login(req, res) {
+  const ssoId = req.body.ssoid;
+  const validationReq = {
+    id: uuid.v4(),
+    method: "GetUser",
+    params: [
+      communicationsKey,
+      ssoId
+    ],
+    jsonrpc: "2.0"
+  };
+  request.post({
+    url: 'https://tunnistus.avoine.fi/mmserver',
+    json: validationReq
+  }, (err, response, body) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Jotain meni pieleen');
+    }
+
+    if (body.error) {
+      console.log(body);
+      req.session = null;
+      return res.status(400).send('Kirjautuminen epäonnistui');
+    }
+
+    const sessionId = uuid.v4();
+    const remoteId = body.result.local_id;
+    return knex('users').where({ remote_id: remoteId })
+      .then((resp) => {
+        if (resp.length === 0) {
+          return Promise.all([
+            sebacon.getUserFirstName(remoteId),
+            sebacon.getUserNickName(remoteId),
+            sebacon.getUserPositions(remoteId),
+            sebacon.getUserDomains(remoteId)
+          ]).then(([firstname, nickname, positions, domains]) => {
+            return knex('users')
+              .insert({
+                remote_id: body.result.local_id,
+                data: {
+                  name: nickname || firstname,
+                  domains: domains.map(d => ({ heading: d, skill_level: 1 })),
+                  positions: positions.map(d => ({ heading: d, skill_level: 1 })),
+                  profile_creation_consented: false
+                }
+              }, 'id') // postgres does not automatically return the id, ask for it explicitly
+          })
+            .then(insertResp => ({ id: insertResp[0] }))
+        } else {
+          return resp[0];
+        }
+      })
+      .then((user) => {
+        return knex('sessions').insert({
+          id: sessionId,
+          user_id: user.id
+        }).then(() => {
+          req.session.id = sessionId;
+          return res.redirect(req.query.path || '/');
+        });
+      })
+  });
+}
+
+function logout(req, res) {
+  const sessionId = req.session.id || 'nosession';
+  req.session = null;
+  return knex('sessions').where({id: sessionId}).del()
+    .then(() => res.redirect('https://tunnistus.avoine.fi/sso-logout/'));
+}
+
+module.exports = {
+  login,
+  logout,
+  initialize
+};
