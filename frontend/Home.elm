d8ba7c8f0d804fb6b2725bec13b98487ac8c7385
Topi Paavilainen <topi.paavilainen@futurice.com>
2017-03-13 16:45:21 +0200

    basic styling for front page

--- frontend/Home.elm
+++ frontend/Home.elm
@@ -1,6 +1,7 @@
 module Home exposing (..)
 
 import Html as H
+import Html.Attributes as A
 
 view : H.Html msg
 view = 
@@ -12,7 +13,14 @@ view =
 introBox : H.Html msg
 introBox =
   H.div
-    []
-    [ H.h3 [] [H.text "Kohtaa tradenomi"]
-    , H.div [] [H.text "Tradenomiitti on tradenomien oma kohtaamispaikka, jossa jäsenet löytävät toisensa yhteisten aiheiden ympäriltä ja hyötyvät toistensa kokemuksista"]
+    [ A.class "home__introbox col-sm-6 col-sm-offset-3" ]
+    [ H.h2 
+      [ A.class "home__introbox--heading" ]
+      [ H.text "Kohtaa tradenomi" ]
+    , H.div
+      [ A.class "home__introbox--content" ] 
+      [ H.text "Tradenomiitti on tradenomien oma kohtaamispaikka, jossa jäsenet löytävät toisensa yhteisten aiheiden ympäriltä ja hyötyvät toistensa kokemuksista." ]
+    , H.button
+      [ A.class "btn btn-primary home__introbox--create-profile-button" ]
+      [ H.text "luo profiili" ]
     ]
