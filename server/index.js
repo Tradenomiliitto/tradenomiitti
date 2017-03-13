e2629a2a514662e9bd507c15c004b2dfd78cb38c
Daniel Landau <daniel.landau@futurice.com>
2017-03-13 15:43:07 +0200

    Extract ads and tweak inversion of control

--- server/index.js
+++ server/index.js
@@ -4,11 +4,6 @@ const uuid = require('uuid');
 const request = require('request');
 const cookieSession = require('cookie-session');
 
-const sebacon = require('./sebaconService');
-const logon = require('./logonHandling');
-const util = require('./util');
-const profile = require('./profile');
-
 const rootDir = "./frontend"
 
 const app = express();
@@ -62,24 +57,23 @@ if (!sebaconAuth ||
   console.warn("You should have SEBACON_* parameters for avoine in ENV");
 }
 
-sebacon.initialize({ customer: sebaconCustomer, user: sebaconUser,
-                     password: sebaconPassword, auth: sebaconAuth});
+const sebacon = require('./sebaconService')({
+  customer: sebaconCustomer, user: sebaconUser,
+  password: sebaconPassword, auth: sebaconAuth});
 
-logon.initialize({ communicationsKey, knex, sebacon });
-util.initialize({ knex });
-profile.initialize({ knex, sebacon, util});
+const logon = require('./logonHandling')({ communicationsKey, knex, sebacon });
+const util = require('./util')({ knex });
+const profile = require('./profile')({ knex, sebacon, util});
+const ads = require('./ads')({ util, knex });
 
 const urlEncoded = bodyParser.urlencoded();
 const jsonParser = bodyParser.json();
 
 app.post('/kirjaudu', urlEncoded, logon.login );
-
 app.get('/uloskirjautuminen', logon.logout);
 
 app.get('/api/profiilit/oma', profile.getMe);
-
 app.put('/api/profiilit/oma', jsonParser, profile.putMe);
-
 app.post('/api/profiilit/luo', profile.consentToProfileCreation);
 
 app.get('/api/tehtavaluokat', (req, res) => {
@@ -90,120 +84,12 @@ app.get('/api/toimialat', (req, res) => {
   return sebacon.getDomainTitles().then(domains => res.json(Object.values(domains)));
 });
 
-app.post('/api/ilmoitukset', jsonParser, (req, res) => {
-  if (!req.session || !req.session.id) {
-    return res.sendStatus(403);
-  }
+app.post('/api/ilmoitukset', jsonParser, ads.createAd);
+app.get('/api/ilmoitukset/:id', ads.getAd);
+app.get('/api/ilmoitukset', ads.listAds);
+app.get('/api/ilmoitukset/tradenomilta/:id', ads.adsForUser);
+app.post('/api/ilmoitukset/:id/vastaus', jsonParser, ads.createAnswer);
 
-  return util.userForSession(req)
-    .then(user => {
-      return knex('ads').insert({
-        user_id: user.id,
-        data: req.body
-      }, 'id');
-    }).then(insertResp => res.json(insertResp[0]));
-});
-
-app.get('/api/ilmoitukset/:id', (req, res) => {
-  return Promise.all([
-    knex('ads').where({id: req.params.id}).first(),
-    util.userForSession(req)
-  ]).then(([ad, user]) => formatAd(ad, user))
-    .then(ad => res.send(ad))
-    .catch(e => { console.error(e); res.sendStatus(404) });
-});
-
-app.get('/api/ilmoitukset', (req, res) => {
-  return Promise.all([
-    knex('ads').where({}),
-    util.userForSession(req)
-  ]).then(([rows, user]) => Promise.all(rows.map(ad => formatAd(ad, user))))
-    .then(ads => ads.sort(latestFirst))
-    .then(ads => res.send(ads))
-});
-
-app.get('/api/ilmoitukset/tradenomilta/:id', (req, res) => {
-  const getAds = knex('ads').where('user_id', req.params.id);
-  const getAnswers = knex('answers').where('user_id', req.params.id).select('ad_id').distinct()
-        .then(results => results.map(o => o.ad_id));
-
-  const getAdsForAnswerer = getAnswers.then(adIds => {
-    return knex('ads').whereIn('id', adIds);
-  });
-  return Promise.all([
-    getAds,
-    getAdsForAnswerer,
-    util.userForSession(req)
-  ]).then(([adsAsAsker, adsAsAnswerer, user]) => {
-    const allAds = adsAsAsker.concat(adsAsAnswerer)
-    return Promise.all(allAds.map(ad => formatAd(ad, user)))
-  }).then(ads => ads.sort(latestFirst))
-    .then(ads => res.send(ads))
-    .catch(err => {
-      console.error(err);
-      res.sendStatus(500);
-    })
-});
-
-//comparing function for two objects with createdAt datestring field. Latest will come first.
-function latestFirst(a, b) {
-  date1 = new Date(a.created_at);
-  date2 = new Date(b.created_at);
-  return date2 - date1;
-}
-
-app.post('/api/ilmoitukset/:id/vastaus', jsonParser, (req, res) => {
-  if (!req.session || !req.session.id) {
-    return res.sendStatus(403);
-  }
-
-  const ad_id = req.params.id;
-
-  return Promise.all([
-    knex('ads').where({ id: ad_id }).first(),
-    knex('answers').where({ ad_id }),
-    util.userForSession(req)
-  ]).then(([ad, answers, user]) => {
-
-    const isAsker = ad.user_id === user.id;
-    if (isAsker) return Promise.reject('User tried to answer own question');
-
-    const alreadyAnswered = answers.some(a => a.user_id === user.id)
-    if (alreadyAnswered) return Promise.reject('User tried to answer several times');
-    return knex('answers').insert({
-      user_id: user.id,
-      ad_id,
-      data: req.body
-    }, 'id');
-  }).then(insertResp => res.json(`${insertResp[0]}`))
-    .catch(err => {
-      console.error('Error in /api/ilmoitukset/:id/vastaus', err);
-      res.sendStatus(500);
-    });
-});
-
-
-function formatAd(ad, user) {
-  return Promise.all([
-    knex('answers').where({ad_id: ad.id})
-      .then(answers => Promise.all(answers.map(formatAnswer))),
-    knex('users').where({id: ad.user_id}).then(rows => rows[0])
-  ]).then(function ([answers, askingUser]) {
-    ad.created_by = util.formatUser(askingUser);
-    ad.answers = user ? answers : answers.length;
-    return ad;
-  })
-}
-
-function formatAnswer(answer) {
-  return knex('users').where({ id: answer.user_id })
-    .then(rows => rows[0])
-    .then(function(user) {
-      answer.created_by = util.formatUser(user);
-      answer.data.content = answer.data.content || '';
-      return answer;
-    })
-}
 
 app.get('*', (req, res) => {
   res.sendFile('./index.html', {root: rootDir})
