029fdd339a283438e7765d8eb01e9d33cdd5156a
Daniel Landau <daniel.landau@futurice.com>
2017-03-14 13:06:29 +0200

    Begin user cards

--- frontend/ListUsers.elm
+++ frontend/ListUsers.elm
@@ -1,7 +1,9 @@
 module ListUsers exposing (..)
 
+import Common
 import Html as H
 import Html.Attributes as A
+import Models.User exposing (User)
 import State.ListUsers exposing (..)
 
 view : Model -> H.Html msg
@@ -24,6 +26,14 @@ view model =
       [ A.class "list-users__list-background"]
       [ H.div
         [ A.class "container" ]
-        [] -- TODO
+        (List.map viewUser model.users)
       ]
+    ]
+
+viewUser : User -> H.Html msg
+viewUser user =
+  H.div
+    [ A.class "user-card col-xs-12 col-sm-6 col-md-3"
+    ]
+    [ Common.authorInfo user
     ]
