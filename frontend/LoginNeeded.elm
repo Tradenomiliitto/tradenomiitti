04ba951dbb5de587c67a6e7439a6edecb5f08730
Daniel Landau <daniel.landau@futurice.com>
2017-03-17 11:26:37 +0200

    Tweak login-needed

--- frontend/LoginNeeded.elm
+++ frontend/LoginNeeded.elm
@@ -13,7 +13,7 @@ view loginUrl =
       [ H.div
         [ A.class "col-xs-11 col-sm-7 center-block login-needed__box" ]
         [ H.h1 [] [ H.text "Kirjaudu sisään" ]
-        , H.p [] [ H.text "Tradenomiitti on ainutlaatuinen ja xxxxxxxxxxxxxxxxxxxxxxxx kohtaamispaikka, joten sinun tulee kirjautua sisään jättääksesi ilmoituksen muille tradenomeille."]
+        , H.p [] [ H.text "Tradenomiitti on ainutlaatuinen kohtaamispaikka Tradenomiliiton jäsenille. Sinun tulee kirjautua sisään TRAL-tunnuksillasi, jotta voit luoda profiilin ja toimia Tradenomiitissa."]
         , H.div
           [ A.class "login-needed__actionable-items" ]
           [ H.div [ A.class "login-needed__actionable-items-join" ]
