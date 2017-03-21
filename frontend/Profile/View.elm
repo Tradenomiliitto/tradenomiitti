699f5d94a8e1307816a23717da3a77636df195a9
Topi Paavilainen <topi.paavilainen@futurice.com>
2017-03-21 13:55:13 +0200

    profile editing page with new layout

--- frontend/Profile/View.elm
+++ frontend/Profile/View.elm
@@ -13,11 +13,133 @@ import State.Main as RootState
 import State.Profile exposing (Model)
 import Models.User exposing (User)
 
-view : Model -> RootState.Model ->  H.Html (AppMessage Msg)
+
+view : Model -> RootState.Model -> H.Html (AppMessage Msg)
 view model rootState =
+  if model.editing
+    then editProfileView model rootState
+    else showProfileView model rootState
+
+
+editProfileView : Model -> RootState.Model -> H.Html (AppMessage Msg)
+editProfileView model rootState =
+  case model.user of
+    Just user ->
+      H.div 
+        []
+        [ profileTopRow model rootState
+        , editProfileHeading
+        , membershipInfoEditing user
+        , H.map LocalMessage (publicInfoEditing model user)
+        , H.map LocalMessage (competences model user)
+        ]
+    Nothing -> H.div [] [] 
+
+
+
+editProfileHeading : H.Html msg
+editProfileHeading =
+  H.div [ A.class "container" ] [
+    H.div [ A.class "row"] [
+  H.div
+    [ A.class "profile__editing--heading col-sm-6 col-sm-offset-3" ]
+    [ H.h2 [ A.class "profile__editing--heading--title" ] [ H.text "Muokkaa profiilia" ]
+    , H.p [ A.class "profile__editing--heading--content" ] [ H.text "Tehdäksemme Tradenomiitin käytöstä sinulle mahdollisimman vaivatonta, olemme luoneet sinulle profiilin TRAL:n jäsentietojen perusteella. Viimeistele profiilisi tarkastamalla jäsentietosi, muokkaamalla julkista profiiliasi ja täyttämällä henkilökohtainen käyntikorttisi."] 
+    ] 
+  ]
+  ]
+
+membershipInfoEditing : User -> H.Html msg
+membershipInfoEditing user =
+  H.div
+    [ A.class "profile__editing--membership container" ]
+      [ H.div
+        [ A.class "row"]
+        [ membershipDataBoxEditing user
+        , membershipDataInfo 
+        ]
+      ]
+
+
+membershipDataInfo : H.Html msg
+membershipDataInfo =
+   H.div
+        [ A.class "profile__editing--membership--info col-md-6" ]
+        [
+          H.p
+            [ A.class "profile__editing--membership--info--text" ] 
+            [ H.text "Profiilissa hyödynnetään liiton jäsentietoja. Tarkistathan, että tietosi ovat järjestelmässämme ajan tasalla. "
+            , H.span [ A.class "profile__editing--bold" ] [H.text "Jäsentiedot eivät näy sellaisenaan muille."
+             ]
+            ]
+        , H.a [ A.href "https://asiointi.tral.fi" ] [ H.button
+            [ A.class "profile__editing--membership--info--button btn btn-primary" ]
+            [ H.text "siirry muokkaamaan jäsentietoja"
+            ] ]
+        ]
+   
+
+
+publicInfoEditing : Model -> User -> H.Html Msg
+publicInfoEditing model user =
+  H.div 
+    [ A.class "container-fluid" ]
+    [ H.div
+        [ A.class "profile__editing--public-info row" ]
+        [ publicInfo model user
+        , businessCard ]
+    ]
+
+publicInfo : Model -> User -> H.Html Msg
+publicInfo model user = 
+  H.div
+    [ A.class "col-sm-6 profile__editing--public-info--box" ]
+    [ H.h3 [A.class "profile__editing--public-info--header"] [H.text "Julkiset tiedot" ]
+    , H.p [A.class "profile__editing--public-info--text"] [H.text "Valitse itsellesi käyttäjänimi (yleisimmin etunimi) ja kuvaava titteli. Esittele itsesi ja osaamisesi muille kuvaavalla tekstillä"]
+    , userInfoBoxEditing model user ]
+
+
+businessCard : H.Html msg
+businessCard =
+  H.div
+    [ A.class "col-sm-6 profile__editing--public-info--box" ]
+    [ H.h3 [A.class "profile__editing--public-info--header"] [H.text "käyntikortti" ]
+    , H.p [A.class "profile__editing--public-info--text"] [ H.text "Täydennä alle tiedot, jotka haluat lähettää käyntikortin mukana. "
+    , H.span [ A.class "profile__editing--bold" ] [ H.text "Tiedot näkyvät vain niille, joille olet lähettänyt kortin" ]
+     ]
+    ]
+
+showProfileView : Model -> RootState.Model ->  H.Html (AppMessage Msg)
+showProfileView model rootState =
   H.div [ A.class "user-page" ] <|
     [ profileTopRow model rootState
     ] ++ (viewUserMaybe model)
+
+
+competences : Model -> User -> H.Html Msg
+competences model user =
+  H.div
+    [ A.class "container profile__editing--competences" ]
+    [ H.div [ A.class "profile__editing--competences--row row" ]
+        [
+          H.div
+            [ A.class "profile__editing--competences--heading col-md-6" ]
+            [ H.h3
+            [ A.class "profile__editing--competences--heading--title" ] 
+            [ H.text "Muokkaa osaamistasi" ]
+        , H.p 
+            [ A.class "profile__editing--competences--heading--text" ]
+            [ H.text "Osaamisesi on esitäytetty jäsentietojemme perusteella. Muokkaa ja täydennä tehtäviä ja toimialoja, jotta Tradenomiitti voi palvella sinua paremmin ja jotta muut tradenomit löytäisivät sinut helpommin. "
+            , H.span [A.class "profile__editing--bold"] [ H.text "Osaaminen näkyy kaikille käyttäjille." ]
+            ]
+        ]
+    ]
+    , H.div
+        [ A.class "profile__editing--competences--row row" ]
+        [ userDomains model user
+        , userPositions model user
+        ]
+    ]
 
 
 profileTopRow : Model -> RootState.Model -> H.Html (AppMessage Msg)
@@ -120,6 +242,49 @@ viewUser model user =
     ]
   ]
 
+
+userInfoBoxEditing2 : User -> H.Html Msg
+userInfoBoxEditing2 user =
+  H.div 
+    [A.class "container"]
+    [ H.div
+        [A.class "row"]
+        [H.div
+          [ A.class " user-page__pic-container col-md-1" ]
+          [ H.span [ A.class "user-page__pic" ] [] ]
+        , H.div [A.class "col-md-4" ]
+          [ H.h4 [ A.class "user-page__name" ]
+            [ 
+                  H.input [ A.placeholder "Miksi kutsumme sinua?"
+                  , A.value user.name
+                  , E.onInput ChangeNickname
+                  ] []
+              ]
+          , H.p
+            [ A.class "user-page__work-details" ]
+            [ 
+                H.input
+                [ A.value user.primaryPosition
+                , E.on "change" (Json.map ChangeTitle E.targetValue)
+                ]
+                []
+            ]
+          ]
+      ]
+    ]
+  
+
+userInfoBoxEditing : Model -> User -> H.Html Msg
+userInfoBoxEditing model user =
+  H.div
+    [ A.class "col-md-6" ]
+    [ H.div
+      [ A.class "row" ]
+      [ userInfoBoxEditing2 user
+      ]
+    , userDescription model user
+    ]
+
 userInfoBox : Model -> User -> H.Html Msg
 userInfoBox model user =
   H.div
@@ -185,6 +350,10 @@ userDomains model user =
     ]
     ([ H.h3 [ A.class "user-page__competences-header" ] [ H.text "Toimiala" ]
     ] ++
+      (if model.editing
+        then [ H.p [A.class "profile__editing--competences--text" ] [H.text "Valitse toimialat, joista olet kiinnostunut tai sinulla on kokemusta" ] ]
+        else [ H.p [ A.class "profile__editing--competences--text" ] [] ])
+     ++
       (List.indexedMap
         (\i x -> H.map (DomainSkillMessage i) <|
           Skill.view model.editing x)
@@ -211,6 +380,10 @@ userPositions model user =
     ]
     ([ H.h3 [ A.class "user-page__competences-header" ] [ H.text "Tehtäväluokka" ]
       ] ++
+        (if model.editing
+          then [ H.p [ A.class "profile__editing--competences--text"] [H.text "Missä tehtävissä olet toiminut tai haluaisit toimia?" ] ]
+          else [ H.p [ A.class "profile__editing--competences--text"] [] ])
+       ++
         (List.indexedMap
           (\i x -> H.map (PositionSkillMessage i) <| Skill.view model.editing x)
           user.positions
@@ -237,25 +410,7 @@ membershipDataBox user =
         [ A.class "col-md-6 user-page__membership-info" ]
         [ H.h3 [ A.class "user-page__membership-info-heading" ] [ H.text "Jäsentiedot:" ]
         , H.span [] [ H.text "(eivät näy muille)"]
-        , H.table
-          [ A.class "user-page__membership-info-definitions" ]
-          [ H.tr []
-              [ H.td [] [ H.text "Kutsumanimi" ]
-              , H.td [] [ H.text extra.nick_name ]
-              ]
-          , H.tr []
-            [ H.td [] [ H.text "Etunimi" ]
-            , H.td [] [ H.text extra.first_name ]
-            ]
-          , H.tr []
-            [ H.td [] [ H.text "Tehtäväluokat" ]
-            , H.td [] [ H.text (String.join ", " extra.positions)]
-            ]
-          , H.tr []
-            [ H.td [] [ H.text "Toimiala" ]
-            , H.td [] [ H.text (String.join ", " extra.domains) ]
-            ]
-          ]
+        , tralInfo extra
         , H.p [] [ H.text "Ovathan jäsentietosi ajan tasalla?" ]
         , H.p [] [ H.a
                      [ A.href "https://asiointi.tral.fi/" ]
@@ -265,5 +420,43 @@ membershipDataBox user =
     Nothing ->
       H.div
         [ A.class "col-md-6 user-page__membership-info" ]
+        [ H.h3 [ A.class "user-page__membership-info-heading" ] [ H.text "Jäsentiedot puuttuvat" ]
+        ]
+
+tralInfo : Models.User.Extra -> H.Html msg
+tralInfo extra =
+  H.table
+    [ A.class "user-page__membership-info-definitions" ]
+    [ H.tr []
+      [ H.td [] [ H.text "Kutsumanimi" ]
+      , H.td [] [ H.text extra.nick_name ]
+      ]
+    , H.tr []
+      [ H.td [] [ H.text "Etunimi" ]
+      , H.td [] [ H.text extra.first_name ]
+      ]
+    , H.tr []
+      [ H.td [] [ H.text "Tehtäväluokat" ]
+      , H.td [] [ H.text (String.join ", " extra.positions)]
+      ]
+    , H.tr []
+      [ H.td [] [ H.text "Toimiala" ]
+      , H.td [] [ H.text (String.join ", " extra.domains) ]
+      ]
+    ]
+
+
+membershipDataBoxEditing : User -> H.Html msg
+membershipDataBoxEditing user =
+  case user.extra of
+    Just extra ->
+      H.div
+        [ A.class "col-md-6 profile__editing--membership--databox" ]
+        [ H.h3 [ A.class "profile__editing--membership--databox--heading" ] [ H.text "TRAL:n  Jäsentiedot" ]
+        , tralInfo extra
+        ]
+    Nothing ->
+      H.div
+        [ A.class "user-page__membership-info" ]
         [ H.h3 [ A.class "user-page__membership-info-heading" ] [ H.text "Jäsentiedot puuttuvat" ]
         ]
