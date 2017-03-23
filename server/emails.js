8c766d352204b7fd2d5514d2ff5e599efcb307bb
Daniel Landau <daniel.landau@futurice.com>
2017-03-23 12:43:31 +0200

    Generate fuller email html

--- server/emails.js
+++ server/emails.js
@@ -1,15 +1,29 @@
 const emailjs = require('emailjs');
+const scssToJson = require('scss-to-json');
+
+const colorsFilepath = __dirname + '/../frontend/stylesheets/colors.scss';
+const scssVars = scssToJson(colorsFilepath);
 
 module.exports = function init(params) {
   const server = emailjs.server.connect(params.smtp);
 
-  function send(user) {
+  function send(user, ad) {
     return (
 `
 <html>
   <head></head>
-  <body>
-    <h1>Terveiset Tradenomiitista sinulle ${user.name}!</h1>
+  <body style="text-align: center; width: 600px; font-family: Arial, sans-serif;">
+    <h1 style="margin-bottom: 50px; color: ${scssVars.$pink}">Tradenomi on vastannut ilmoitukseesi</h1>
+    <p>Ilmoitus voi tuoda mukanaan uusia arvokkaita kontakteja. Muista lähettää kiinnostaville tradenomeille yksityisviesti ja/tai käyntikortti.</p>
+    <p style="margin-top: 80px;">
+      <a style="text-transform: uppercase; background-color: ${scssVars.$pink}; padding-left: 45px; padding-right: 45px; padding-top: 25px; padding-bottom: 25px; color: ${scssVars.$white}; text-decoration: none;" href="">Katso vastaus</a>
+    </p>
+    <h4 style="font-weight: bold; text-transform: uppercase; margin-top: 100px;">Ilmoituksesi</h4>
+    <div style="width: 80%; background-color: ${scssVars['$light-grey-background']}; border-color: ${scssVars['$medium-grey']}; border-style: solid; border-width: 1px; padding: 30px; margin-left: auto; margin-right: auto;">
+      <h2 style="color: ${scssVars.$pink};">${ad.heading}</h2>
+      <p>${ad.content}</p>
+    </div>
+    <p style="margin-top: 50px;">Etkö halua enää sähköposteja? Voit muokata sähköpostiasetuksia <a href="https://tradenomiitti.fi/profiili" style="text-decoration: none;">Profiili-sivulla</a>.</p>
   </body>
 </html>
 `
