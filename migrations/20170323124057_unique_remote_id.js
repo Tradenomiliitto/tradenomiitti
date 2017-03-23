dd19128e4801c22c2690d6f5cc2ff47e26977b65
Topi Paavilainen <topi.paavilainen@futurice.com>
2017-03-23 14:24:30 +0200

    make remote_id unique, add cascade rules on ads and answers

---
+++ migrations/20170323124057_unique_remote_id.js
@@ -0,0 +1,36 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.alterTable('users', function(table){
+      table.unique('remote_id');
+    }),
+    knex.schema.alterTable('ads', function(table){
+      table.dropForeign('user_id');
+      table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
+    }),
+    knex.schema.alterTable('answers', function(table){
+      table.dropForeign('ad_id');
+      table.foreign('ad_id').references('id').inTable('ads').onDelete('CASCADE');
+      table.dropForeign('user_id');
+      table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.alterTable('users', function(table){
+      table.dropUnique('remote_id');
+    }),
+    knex.schema.alterTable('ads', function(table){
+      table.dropForeign('user_id');
+      table.foreign('user_id').references('id').inTable('users');
+    }),
+    knex.schema.alterTable('answers', function(table){
+      table.dropForeign('ad_id');
+      table.foreign('user_id').references('id').inTable('ads');
+      table.dropForeign('user_id');
+      table.foreign('user_id').references('id').inTable('users');
+    })
+  ]);
+};
