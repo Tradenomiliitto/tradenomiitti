e012ef7601530ab7a83066e32acc1d3998c61663
Daniel Landau <daniel.landau@futurice.com>
2017-03-28 16:26:37 +0300

    Keep track of scroll position

--- frontend/js/scrolling.js
+++ frontend/js/scrolling.js
@@ -2,13 +2,29 @@ import { polyfill } from 'smoothscroll-polyfill';
 
 polyfill();
 
+const previousScrolls = {};
+window.history.scrollRestoration = 'manual';
+
+window.addEventListener('scroll', (e) => {
+  const path = document.location.pathname
+  previousScrolls[path] = window.scrollY;
+});
+
 export default function initScrolling(port) {
   port.subscribe(shouldScroll => {
+    const path = document.location.pathname
     if (shouldScroll) {
+      previousScrolls[path] = 0;
       window.scroll({
         top: 0,
         behavior: 'smooth'
       });
+    } else {
+      setTimeout(() => {
+        window.scroll({
+          top: previousScrolls[path]
+        });
+      }, 0)
     }
   });
 }
