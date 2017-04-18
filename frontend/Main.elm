ce88fb0627b3578f9c20ae3273072c59840cc8af
Daniel Landau <daniel.landau@futurice.com>
2017-04-18 22:37:04 +0300

    Switch to BETA logo

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -434,7 +434,7 @@ logoImage : Int -> H.Html msg
 logoImage width =
   H.img
     [ A.alt "Tradenomiitti"
-    , A.src "/static/tradenomiitti_logo.svg"
+    , A.src "/static/tradenomiitti_BETA_logo.svg"
     , A.class "logo-image"
     , A.width width
     ]
