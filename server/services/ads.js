743ac65ea225aae18ce09a3b373bb6110a0dce8a
sevesalm <severi.salminen@iki.fi>
2017-07-05 13:18:16 +0300

    Simplified the query statements a bit.

--- server/services/ads.js
+++ server/services/ads.js
@@ -8,12 +8,12 @@ module.exports = function initialize(params) {
 
     switch(sorting) {
       case 'created_at_asc':
-        query = query.orderBy('created_at', 'asc');
+        query.orderBy('created_at', 'asc');
         break;
       case 'answers_desc':
         // Ads with most answers first. If equal, newest first.
         answers.select('ad_id').count('* as count').groupBy('ad_id').as('answers');
-        query = query.leftOuterJoin(answers, 'answers.ad_id', 'ads.id').orderByRaw('count DESC NULLS LAST').orderBy('created_at', 'desc');
+        query.leftOuterJoin(answers, 'answers.ad_id', 'ads.id').orderByRaw('count DESC NULLS LAST').orderBy('created_at', 'desc');
         break;
       case 'answers_asc':
         // Ads with least answers first. If equal, newest first.
@@ -23,23 +23,23 @@ module.exports = function initialize(params) {
       case 'newest_answer_desc':
         // Ads with the newest answer first. If equal, newest first.
         answers.select('ad_id').max('created_at as newest_answer').groupBy('ad_id').as('answers');
-        query = query.leftOuterJoin(answers, 'answers.ad_id', 'ads.id').orderByRaw('newest_answer DESC NULLS LAST').orderBy('created_at', 'desc');
+        query.leftOuterJoin(answers, 'answers.ad_id', 'ads.id').orderByRaw('newest_answer DESC NULLS LAST').orderBy('created_at', 'desc');
         break;
       default:
-        query = query.orderBy('created_at', 'desc');
+        query.orderBy('created_at', 'desc');
         break;
     }
 
-    if (limit !== undefined) query = query.limit(limit);
-    if (offset !== undefined) query = query.offset(offset);
+    if (limit !== undefined) query.limit(limit);
+    if (offset !== undefined) query.offset(offset);
     if (domain !== undefined) {
-      query = query.whereRaw("data->>'domain' = ?", [ domain ])
+      query.whereRaw("data->>'domain' = ?", [ domain ])
     }
     if (position !== undefined) {
-      query = query.whereRaw("data->>'position' = ?", [ position ])
+      query.whereRaw("data->>'position' = ?", [ position ])
     }
     if (location !== undefined) {
-      query = query.whereRaw("data->>'location' = ?", [ location ])
+      query.whereRaw("data->>'location' = ?", [ location ])
     }
     return query
       .then(rows => Promise.all(rows.map(ad => util.formatAd(ad, loggedIn))))
