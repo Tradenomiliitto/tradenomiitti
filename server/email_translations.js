20a983150c38c53495a1d6e24d64ce85f692b9f4
Severi Salminen <severi.salminen@futurice.com>
2017-07-28 11:51:07 +0300

    Fixed some bugs. Moved translatable strings into json.

---
+++ server/email_translations.js
@@ -0,0 +1,42 @@
+const source = {
+  sendNotificationForAnswer: {
+    text: 'Kirjaudu Tradenomiittiin nähdäksesi vastauksen',
+    subject: 'Ilmoitukseesi on vastattu',
+  },
+  sendNotificationForContact: {
+    text: 'Kirjaudu Tradenomiittiin nähdäksesi kontaktin profiilin',
+    subject: 'Olet saanut uuden kontaktin',
+  },
+  sendNotificationForAds: {
+    text: 'Kirjaudu Tradenomiittiin nähdäksesi uusimman sisällön',
+    subject: 'Uusia ilmoituksia Tradenomiitissa',
+  },
+  answerNotificationHtml: {
+    h1: 'Tradenomi on vastannut ilmoitukseesi',
+    p1: 'Ilmoitus voi tuoda mukanaan uusia arvokkaita kontakteja. Muista lähettää kiinnostaville tradenomeille yksityisviesti ja/tai käyntikortti.',
+    a1: 'Katso vastaus',
+    h4: 'Ilmoituksesi',
+    p2: 'Etkö halua enää sähköposteja? Voit muokata sähköpostiasetuksiasi',
+    a2: 'Käyttäjätilin asetuksista',
+  },
+  contactNotificationHtml: {
+    h1: 'Olet saanut uuden kontaktin',
+    p1: 'Toinen tradenomi on antanut sinulle käyntikorttinsa Tradenomiitti-palvelussa. Voitte nyt olla yhteydessä ja jakaa osaamistanne vaikka kasvotusten.',
+    p2: 'Profiilisivun kautta voit lähettää oman käyntikorttisi.',
+    a1: 'Katso profiili',
+    p3: 'Etkö halua enää sähköposteja? Voit muokata sähköpostiasetuksiasi',
+    a2: 'Käyttäjätilin asetuksista',
+  },
+  singleAdHtml: {
+    categoriesText: 'Kaikki tradenomit',
+    a: 'Vastaa ilmoitukseen',
+  },
+  adNotificationHtml: {
+    h1: 'Sinulta odotetaan vastausta',
+    p1: 'Toinen tradenomi on jättänyt ilmoituksen ja toivoo vastausta sinun kaltaiseltasi osaajalta. Auta vertaistasi jakamalla näkemyksesi.',
+    p2: 'Etkö halua enää sähköposteja? Voit muokata sähköpostiasetuksiasi',
+    a: 'Käyttäjätilin asetuksista',
+  },
+};
+
+module.exports = source;
