d56a91143bc7d20e5e377ede39ed45ec3de82d37
Severi Salminen <severi.salminen@futurice.com>
2017-09-18 11:50:15 +0300

    Show errors below the form

--- frontend/RenewPassword.elm
+++ frontend/RenewPassword.elm
@@ -46,58 +46,78 @@ update msg model =
         Email email ->
             { model | email = email } ! []
 
-        SendResponse (Err error) ->
-            { model | status = Failure } ! []
-
         SendResponse (Ok response) ->
             { model | status = Success } ! []
 
+        SendResponse (Err httpError) ->
+            let
+                error =
+                    case httpError of
+                        Http.BadStatus _ ->
+                            Failure
+
+                        _ ->
+                            NetworkError
+            in
+            { model | status = error } ! []
+
         Submitted ->
             model ! [ Cmd.map LocalUpdateMessage <| submit model ]
+
+
+renewPasswordForm : T -> Model -> Maybe String -> H.Html Msg
+renewPasswordForm t model errorMessage =
+    H.div
+        [ A.class "container last-row" ]
+        [ H.div
+            [ A.class "row renewpassword col-sm-6 col-sm-offset-3" ]
+            [ H.form
+                [ A.class "renewpassword__container"
+                , onWithOptions "submit"
+                    { preventDefault = True, stopPropagation = False }
+                    (Json.Decode.succeed Submitted)
+                ]
+                [ H.h1
+                    [ A.class "renewpassword__heading" ]
+                    [ H.text <| t "renewPassword.title" ]
+                , H.p [] [ H.text <| t "renewPassword.hint" ]
+                , H.h3
+                    [ A.class "renewpassword__input" ]
+                    [ H.input
+                        [ A.name "email"
+                        , A.type_ "email"
+                        , A.autofocus True
+                        , A.placeholder <|
+                            t "renewPassword.emailPlaceholder"
+                        , onInput Email
+                        ]
+                        []
+                    ]
+                , errorMessage
+                    |> Maybe.map
+                        (\message ->
+                            H.p [ A.class "error" ] [ H.text message ]
+                        )
+                    |> Maybe.withDefault (H.text "")
+                , H.p
+                    [ A.class "renewpassword__submit-button" ]
+                    [ H.button
+                        [ A.type_ "submit"
+                        , A.class "btn btn-primary"
+                        , A.disabled (String.length model.email == 0)
+                        ]
+                        [ H.text <| t "renewPassword.buttonText" ]
+                    ]
+                ]
+            ]
+        ]
 
 
 view : T -> Model -> H.Html Msg
 view t model =
     case model.status of
         NotLoaded ->
-            H.div
-                [ A.class "container last-row" ]
-                [ H.div
-                    [ A.class "row renewpassword col-sm-6 col-sm-offset-3" ]
-                    [ H.form
-                        [ A.class "renewpassword__container"
-                        , onWithOptions "submit"
-                            { preventDefault = True, stopPropagation = False }
-                            (Json.Decode.succeed Submitted)
-                        ]
-                        [ H.h1
-                            [ A.class "renewpassword__heading" ]
-                            [ H.text <| t "renewPassword.title" ]
-                        , H.p [] [ H.text <| t "renewPassword.hint" ]
-                        , H.h3
-                            [ A.class "renewpassword__input" ]
-                            [ H.input
-                                [ A.name "email"
-                                , A.type_ "email"
-                                , A.autofocus True
-                                , A.placeholder <|
-                                    t "renewPassword.emailPlaceholder"
-                                , onInput Email
-                                ]
-                                []
-                            ]
-                        , H.p
-                            [ A.class "renewpassword__submit-button" ]
-                            [ H.button
-                                [ A.type_ "submit"
-                                , A.class "btn btn-primary"
-                                , A.disabled (String.length model.email == 0)
-                                ]
-                                [ H.text <| t "renewPassword.buttonText" ]
-                            ]
-                        ]
-                    ]
-                ]
+            renewPasswordForm t model Nothing
 
         Success ->
             H.div
@@ -114,15 +134,9 @@ view t model =
                 ]
 
         Failure ->
-            H.div
-                [ A.class "container last-row" ]
-                [ H.div
-                    [ A.class "row renewpassword col-sm-6 col-sm-offset-3" ]
-                    [ H.div
-                        [ A.class "renewpassword__container" ]
-                        [ H.h1
-                            [ A.class "changepassword__heading" ]
-                            [ H.text <| t "renewPassword.failure" ]
-                        ]
-                    ]
-                ]
+            renewPasswordForm t model <|
+                Just (t "renewPassword.failure")
+
+        NetworkError ->
+            renewPasswordForm t model <|
+                Just (t "renewPassword.networkError")
