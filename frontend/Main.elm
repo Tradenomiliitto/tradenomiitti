28bec473a0c4530891a7f770b0c3c6dfd12579f8
Severi Salminen <severi.salminen@futurice.com>
2018-02-23 11:33:47 +0200

    Remove beta label

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -668,9 +668,6 @@ logo t =
             ]
             [ H.span [] [ H.text "MiBiT" ]
             ]
-        , H.span [ A.class "navbar-brand__beta" ]
-            [ H.text "BETA"
-            ]
         ]
 
 
